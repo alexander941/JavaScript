b8a02564cc1a932cadd22a52deb391411804663e
Alexander <36586728+alexander941@users.noreply.github.com>
2020-06-29 22:04:30 +0300

    Add files via upload

---
+++ Lesson4/script.js
@@ -0,0 +1,9 @@
+confirm("Нажмите кнопку для замены текста страницы");
+
+function key(){
+    let value1 = 5, 
+    value2 = 9, 
+    sum = value1 + value2;
+    let title = document.getElementById('title');
+    title.innerText = sum;
+}
