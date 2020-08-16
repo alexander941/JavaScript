608f22122176a044d33b1d3003b5b6ed48e6a03c
Alexander <36586728+alexander941@users.noreply.github.com>
2020-08-16 20:47:19 +0300

    Add files via upload

---
+++ Freeman, Robson book/Lesson33/script.js
@@ -0,0 +1,7 @@
+var input =  "jenny@wickedlysmart.com";
+for(var i = 0;i < input.length;i++){
+    if(input.charAt(i) === "@"){
+        console.log("There's an @ sign at index " + i);
+    }
+    
+}
