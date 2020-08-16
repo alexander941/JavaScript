7bb09f6026a6f0968fa3baba6195b2f5dbead97e
Alexander <36586728+alexander941@users.noreply.github.com>
2020-08-16 20:47:56 +0300

    Add files via upload

---
+++ Freeman, Robson book/Lesson35/script.js
@@ -0,0 +1,10 @@
+var data = "name|phone|address";
+var val = data.substring(5, 10);
+var vals = data.split("|");
+
+val = data.substring(5);
+var vals = data.split("|");
+
+console.log("Substring is " + val);
+console.log("Substring is now " + val);
+console.log("Split array is ", vals);
