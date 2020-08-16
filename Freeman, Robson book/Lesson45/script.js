2a91b7e4bda8db777e3ee6d4fccee3c3c9747ae4
Alexander <36586728+alexander941@users.noreply.github.com>
2020-08-17 00:54:56 +0300

    Add files via upload

---
+++ Freeman, Robson book/Lesson45/script.js
@@ -0,0 +1,14 @@
+function quack(num){
+    for(var i = 0; i < num;i++){
+        console.log("Quack!");
+    }
+}
+var fly = function(num){
+    for(i = 0;i < num;i++){
+        console.log("Flying!");
+    }
+}
+var superFly = fly;
+superFly(2);
+var superQuack = quack;
+superQuack(4);
