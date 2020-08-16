daf00bbecfbfe870a69658af24fbe1d698a857da
Alexander <36586728+alexander941@users.noreply.github.com>
2020-08-17 00:59:32 +0300

    Add files via upload

---
+++ Freeman, Robson book/Lesson58/scrpt.js
@@ -0,0 +1,6 @@
+var taxi = new CarPrototio();
+taxi.model = "Bmw 5g30";
+taxi.color = "Black";
+taxi.currentTime = new Date();
+taxi.fluxCapacitor = {type:"Mr. Fusion"};
+taxi.timeTravel = function(date){};
